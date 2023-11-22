c7c816578ecf4c401d267f165f431f9b80a3b559
PinappleJuice23 <105725448+PinappleJuice23@users.noreply.github.com>
2023-11-22 23:00:23 +0100

    Add files via upload

---
+++ xml_file_processor.py
@@ -0,0 +1,69 @@
+import xml.etree.ElementTree as ET
+
+class FileProcessor:
+    def read_file(self, filename):
+        try:
+            tree = ET.parse(filename)
+            return tree.getroot()
+        except FileNotFoundError:
+            print("File not found.")
+            return None
+        except ET.ParseError:
+            print("Error parsing XML file.")
+            return None
+
+    def add_record(self, filename, record):
+        try:
+            tree = ET.parse(filename)
+            root = tree.getroot()
+            root.append(record)
+            tree.write(filename)
+            print("Succes!")
+        except FileNotFoundError:
+            print("File not found.")
+        except ET.ParseError:
+            print("Error parsing XML file.")
+
+    def delete_record(self, filename, record_id):
+        try:
+            tree = ET.parse(filename)
+            root = tree.getroot()
+            for record in root.findall('.//record'):
+                if record.find('id').text == record_id:
+                    root.remove(record)
+                    tree.write(filename)
+                    print("Succes!")
+                    return
+            print("Record not found.")
+        except FileNotFoundError:
+            print("File not found.")
+        except ET.ParseError:
+            print("Error parsing XML file.")
+
+    def update_record(self, filename, record_id, new_record):
+        try:
+            tree = ET.parse(filename)
+            root = tree.getroot()
+            for record in root.findall('.//record'):
+                if record.find('id').text == record_id:
+                    root.remove(record)
+                    root.append(new_record)
+                    tree.write(filename)
+                    print("Record updated successfully.")
+                    return
+            print("Record not found.")
+        except FileNotFoundError:
+            print("File not found.")
+        except ET.ParseError:
+            print("Error parsing XML file.")
+
+    def display_records(self, filename):
+        try:
+            tree = ET.parse(filename)
+            root = tree.getroot()
+            for record in root.findall('.//record'):
+                print(ET.tostring(record).decode())
+        except FileNotFoundError:
+            print("File not found.")
+        except ET.ParseError:
+            print("Error parsing XML file.")
